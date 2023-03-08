9304eb4bc24a21c854bb003875486b2d991271e6
MineGamer69 <aaryankapoor2003@gmail.com>
2023-03-08 15:24:58 -0500

    fixed the network permissions

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.assignment3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val apiInterface = ApiInterface.create().getMoviebyname("skyfall","us")
+
+        if (apiInterface != null) {
+            apiInterface.enqueue( object : Callback<ArrayList<MovData?>?> {
+                override fun onResponse(
+                    call: Call<ArrayList<MovData?>?>?,
+                    response: Response<ArrayList<MovData?>?>
+                ) {
+                    if (response != null) {
+                        Log.d("Main activity", response.message())
+                        Log.d("Main activity", response.headers().toString())
+                        Log.d("Main activity", response.body().toString())
+                    }
+
+                }
+
+                override fun onFailure(call: Call<ArrayList<MovData?>?>, t: Throwable) {
+                    if (t != null) {
+                        t.message?.let { Log.d("onFailure", it) }
+                    }
+                }
+            })
+        }
+
     }
 }
