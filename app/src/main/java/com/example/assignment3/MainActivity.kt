ac14156e7241fed6fd9d9efab2a7ead50e7c17f2
MineGamer69 <aaryankapoor2003@gmail.com>
2023-03-14 15:15:51 -0400

    added intro screen and made it so the input form user is api call now

--- app/src/main/java/com/example/assignment3/MainActivity.kt
+++ app/src/main/java/com/example/assignment3/MainActivity.kt
@@ -11,29 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val apiInterface = ApiInterface.create().getMoviebyname("skyfall","us")
-
-        if (apiInterface != null) {
-            apiInterface.enqueue( object : Callback<ArrayList<MovData?>?> {
-                override fun onResponse(
-                    call: Call<ArrayList<MovData?>?>?,
-                    response: Response<ArrayList<MovData?>?>
-                ) {
-                    if (response != null) {
-                        Log.d("Main activity", response.message())
-                        Log.d("Main activity", response.headers().toString())
-                        Log.d("Main activity", response.body().toString())
-                    }
-
-                }
-
-                override fun onFailure(call: Call<ArrayList<MovData?>?>, t: Throwable) {
-                    if (t != null) {
-                        t.message?.let { Log.d("onFailure", it) }
-                    }
-                }
-            })
-        }
-
     }
 }
